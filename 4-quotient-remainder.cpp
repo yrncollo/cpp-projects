09453e0827939f5a28dac8cac4db2caee385c863
yrncollo <ngenocollins599@gmail.com>
2023-01-31 16:17:06 +0300

    C++ Program to Find Quotient and Remainder

---
+++ 4-quotient-remainder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num, num2, result;
+	cout << "Enter an integer: ";
+	cin >> num >> num2;
+	result = num % num2 ;
+	cout << result;
+	return 0;
+}
