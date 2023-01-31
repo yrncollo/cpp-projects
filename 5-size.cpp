f39a0aa801152cde47edd7f9c9ab6e5a82550879
yrncollo <ngenocollins599@gmail.com>
2023-01-31 16:38:51 +0300

    C++ Program to Find Size of int, float, double and char in Your System

---
+++ 5-size.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	cout << "size of char is: " << sizeof(char) << "\n";
+	cout << "size of int is: " << sizeof(int) << "\n";
+	cout << "size of double is: " << sizeof(double) << "\n";
+	cout << "size of float is: " << sizeof(float) << "\n";
+
+	return 0;
+}
