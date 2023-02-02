bd4bf7d8a071fdc7655ef5ef7fbef50317bfc30c
yrncollo <ngenocollins599@gmail.com>
2023-02-02 12:27:03 +0300

    C++ Program to Calculate Sum of Natural Numbers

---
+++ 11-sum-of-natural-num.cpp
@@ -0,0 +1,14 @@
+// to find the sum of natural numbers we use the formula:
+// S=n(n+1)2
+#include <iostream>
+
+using namespace std;
+int main(){
+	int n,S;
+	cout << "Enter the n value: ";
+	cin >> n;
+	S = n*(n+1)/2 ;
+	cout << S;
+
+	return 0;
+}
