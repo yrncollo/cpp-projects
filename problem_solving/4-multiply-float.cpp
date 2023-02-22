e20ecd12af4f55889004c88ebe92092f410e631a
yrncollo <ngenocollins599@gmail.com>
2023-02-22 19:11:36 +0300

    Multiply two Floating Point Numbers

---
+++ problem_solving/4-multiply-float.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,result;
+
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+
+	result = a * b;
+	cout << "The result is: "<< result;
+
+	return 0;
+}
