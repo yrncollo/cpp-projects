8a6e5b33fc864234af76d17523bdae3cb483bab2
yrncollo <ngenocollins599@gmail.com>
2023-02-22 19:07:26 +0300

    Add Two Integers

---
+++ problem_solving/3-add-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a,b, sum;
+	std::cout << "Enter two integers: ";
+	std::cin >> a >> b;
+
+	sum = a + b;
+	std::cout <<"The sum is: " << sum;
+
+	return 0;
+}
