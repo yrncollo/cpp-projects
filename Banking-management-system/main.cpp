a56fe64255c15f22e37af76c124ae80d5946c87c
yrncollo <ngenocollins599@gmail.com>
2023-02-23 18:28:30 +0300

    created welcome function

---
+++ Banking-management-system/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+void welcome();
+
+int main()
+{
+
+	welcome();
+
+	return 0;
+}
+
+void welcome()
+{
+	std::cout << "|-----------------------------------------------|" << "\n";
+	std::cout << "|                                               |" << "\n"; 
+	std::cout << "|     WELCOME TO YRNCOLLO BANKING SYSTEM        |" << "\n";
+	std::cout << "|                                               |" << "\n"; 
+	std::cout << "|-----------------------------------------------|" << "\n";
+}
