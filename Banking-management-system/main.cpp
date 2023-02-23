84bbf47002f1c20790a3c96700993270c2115190
yrncollo <ngenocollins599@gmail.com>
2023-02-23 19:03:01 +0300

    customized to look better

--- Banking-management-system/main.cpp
+++ Banking-management-system/main.cpp
@@ -11,9 +11,24 @@ int main()
 
 void welcome()
 {
-	std::cout << "|-----------------------------------------------|" << "\n";
-	std::cout << "|                                               |" << "\n"; 
-	std::cout << "|     WELCOME TO YRNCOLLO BANKING SYSTEM        |" << "\n";
-	std::cout << "|                                               |" << "\n"; 
-	std::cout << "|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|     WELCOME TO YRNCOLLO BANKING SYSTEM        |" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|\t Choose your option:\t                |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 1) ADMINISTRATOR               |" << "\n"; 
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 2) LOGIN                       |" << "\n"; 
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 3) EXIT                        |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+
+	int choice;
+	std::cout << "\n\nEnter: ";
+	std::cin >> choice;
+	
+	
 }
