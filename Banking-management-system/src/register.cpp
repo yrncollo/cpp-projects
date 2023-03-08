20915ff941e5622695f10e0a0326322b5a82b903
yrncollo <ngenocollins599@gmail.com>
2023-03-08 12:54:18 +0300

    created admin func

--- Banking-management-system/src/register.cpp
+++ Banking-management-system/src/register.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "main.h"
 
@@ -20,9 +21,32 @@ void welcome()
 	std::cout << "\t\t|\t\t 3) EXIT                        |" << "\n"; 
 	std::cout << "\t\t|-----------------------------------------------|" << "\n";
 
-//	int choice;
-//	std::cout << "\n\nEnter: ";
-//	std::cin >> choice;
+	int choice;
+	std::cout << "\n\nEnter: ";
+	std::cin >> choice;
 	
+	switch (choice) {
+		case 1:
+			administrator();
 	
+	}
+	
+}
+
+void administrator()
+{
+	std::string admPassword;
+	std::string admUsername ;
+	std::cout << "Enter Administrator Username: ";
+	std::cin >> admUsername;
+	std::cout << "Enter the password: ";
+	std::cin >> admPassword;
+	if (admPassword == "WelcomeAdmin" && admUsername == "admin") {
+	
+		std::cout << "\n Login successful";
+	}
+	else {
+		std::cout << "\n Username or Password is invalid";
+	}
+
 }
