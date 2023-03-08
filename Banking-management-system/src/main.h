75e00c03064385d1f5a36e75b153082c8f68b818
yrncollo <ngenocollins599@gmail.com>
2023-03-08 13:39:20 +0300

    added struct for details

--- Banking-management-system/src/main.h
+++ Banking-management-system/src/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string>
 void welcome();
 void administrator();
+void login();
+
+struct details
+{
+	std::string username;
+	std::string password;
+
+};
 #endif
