293b1337966172742bcb81ef720c6e80d9b26f7d
yrncollo <ngenocollins599@gmail.com>
2023-03-08 12:15:32 +0300

    moved the welcome function to register.cpp

---
+++ Banking-management-system/src/register.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "main.h"
+
+
+void welcome()
+{
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|     WELCOME TO YRNCOLLO BANKING SYSTEM        |" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|\t Choose your option:\t                |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 1) ADMINISTRATOR               |" << "\n"; 
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 2) LOGIN                       |" << "\n"; 
+	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t|\t\t 3) EXIT                        |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n";
+
+//	int choice;
+//	std::cout << "\n\nEnter: ";
+//	std::cin >> choice;
+	
+	
+}
