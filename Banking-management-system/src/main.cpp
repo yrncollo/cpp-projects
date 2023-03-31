edbef54a5533772de52b03e5db45db6abfd356c9
yrncollo <81387575+yrncollo@users.noreply.github.com>
2023-03-31 20:21:51 +0300

    Removing duplicate files (#25)
    * should now remove .exe files
    
    * changed the main.cpp
    
    * removed main.h file
    
    * removed register.cpp file
    
    * moved to src directory
    
    * removed the binary file
    
    * added how to contribute
    
    * added how to contribute to the readme file
    
    * Fix: removed a line

--- Banking-management-system/src/main.cpp
+++ Banking-management-system/src/main.cpp
@@ -1,9 +1,305 @@
-#include "main.h"
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <conio.h>
+
+using namespace std;
+
+class account
+{
+private:
+    string name = "null";
+    int balance = 0, withdraw, deposit;
+    string pin = "";
+    char ch;
+
+public:
+    void CreateAccount()
+    {
+        if (name != "null")
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nAccount has Already been created" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+
+        else
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+
+            cout << "\nEnter your Full Name :- ";
+            getline(cin, name);
+
+            cout << "\nSet a Pin :- ";
+            while ((ch = getch()) != '\r')
+            {
+                if (ch == '\b')
+                {
+                    if (pin.length() > 0)
+                    {
+                        pin.pop_back();
+                        cout << "\b \b";
+                    }
+                }
+                else
+                {
+                    pin.push_back(ch);
+                    cout << "*";
+                }
+            }
+
+            cout << "\n\n***** Account has been created succesfully *****" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+    }
+
+    void ChangePin()
+    {
+        if (name != "null")
+        {
+            string extpin = "";
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nEnter your existing pin :- ";
+            while ((ch = getch()) != '\r')
+            {
+                if (ch == '\b')
+                {
+                    if (extpin.length() > 0)
+                    {
+                        extpin.pop_back();
+                        cout << "\b \b";
+                    }
+                }
+                else
+                {
+                    extpin.push_back(ch);
+                    cout << "*";
+                }
+            }
+
+            if (extpin == pin)
+            {
+                cout << "\nEnter New Pin :- ";
+                pin = "";
+                while ((ch = getch()) != '\r')
+                {
+                    if (ch == '\b')
+                    {
+                        if (pin.length() > 0)
+                        {
+                            pin.pop_back();
+                            cout << "\b \b";
+                        }
+                    }
+                    else
+                    {
+                        pin.push_back(ch);
+                        cout << "*";
+                    }
+                }
+
+                cout << "\n***** Pin Changed succesfully *****" << endl;
+                cout << "\n-----------------------" << endl;
+                system("pause");
+            }
+            else
+            {
+                system("cls");
+                cout << "-----------------------" << endl;
+                cout << "\nPin does not match please try again" << endl;
+                cout << "\n-----------------------" << endl;
+                system("pause");
+            }
+        }
+        else
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nNo Account has been created yet \n First Create one account" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+    }
+
+    void PrintBalance()
+    {
+        if (name != "null")
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nAccount Holder Name :- " << name << endl;
+            cout << "Your Savings Account Balance is :- " << balance << " Rupees " << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+        else
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nNo Account has been created yet \n First Create one account" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+    }
+
+    void AddMoney()
+    {
+        if (name != "null")
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nHow much money do you want to add :- ";
+            cin >> deposit;
+            cin.ignore();
+            balance = balance + deposit;
+
+            cout << "\nRupees " << deposit << " has been added to your account " << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+        else
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nNo Account has been created yet \n First Create one account" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+    }
+
+    void WithdrawMoney()
+    {
+        string extpin = "";
+
+        if (name == "null")
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\nNo Account has been created yet \n First Create one account" << endl;
+            cout << "\n-----------------------" << endl;
+            system("pause");
+        }
+
+        else
+        {
+            system("cls");
+            cout << "-----------------------" << endl;
+            cout << "\n Enter your pin :- ";
+            while ((ch = getch()) != '\r')
+            {
+                if (ch == '\b')
+                {
+                    if (extpin.length() > 0)
+                    {
+                        extpin.pop_back();
+                        cout << "\b \b";
+                    }
+                }
+                else
+                {
+                    extpin.push_back(ch);
+                    cout << "*";
+                }
+            }
+            cout << "\n-----------------------" << endl;
+
+            if (name != "null" && extpin == pin)
+            {
+                system("cls");
+                cout << "-----------------------" << endl;
+                cout << "\nHow much money do you want to withdraw :- ";
+                cin >> withdraw;
+                cin.ignore();
+
+                if (withdraw <= balance)
+                {
+                    balance = balance - withdraw;
+                    cout << "\nRupees " << withdraw << " has been withdrawn from your account " << endl;
+                }
+                else
+                {
+                    cout << "\n***** Not Enough money in account ***** \nDo you want to take Loan :)" << endl;
+                }
+                cout << "\n-----------------------" << endl;
+                system("pause");
+            }
+            if (name != "null" && extpin != pin)
+            {
+                system("cls");
+                cout << "-----------------------" << endl;
+                cout << "\nPin does not match please try again" << endl;
+                cout << "\n-----------------------" << endl;
+                system("pause");
+            }
+        }
+    }
+};
+
 int main()
 {
-	welcome();
+    int u;
+    account obj;
 
-	return 0;
+    do
+    {
+        system("cls");
+
+        cout << "-----------------------" << endl;
+        cout << " Welcome to Kunal's ATM Banking" << endl;
+        cout << "-----------------------" << endl;
+
+        cout << "\n-----------------------" << endl;
+        cout << "\nChoose option by pressing option number eg.1,2,3.." << endl;
+
+        cout << "1.Open a Savings Account" << endl;
+        cout << "2.Change Pin of your Savings Account" << endl;
+        cout << "3.Show Balance in Savings Account" << endl;
+        cout << "4.Add Money to Savings Account" << endl;
+        cout << "5.Withdraw Money form Savings Account" << endl;
+
+        cout << "6.Logout & Exit" << endl;
+        cout << "\n-----------------------" << endl;
+
+        cout << "\n>>";
+        cin >> u;
+        cin.ignore();
+
+        if (u == 1)
+        {
+            obj.CreateAccount();
+        }
+
+        if (u == 2)
+        {
+            obj.ChangePin();
+        }
+
+        if (u == 3)
+        {
+            obj.PrintBalance();
+        }
+
+        if (u == 4)
+        {
+            obj.AddMoney();
+        }
+
+        if (u == 5)
+        {
+            obj.WithdrawMoney();
+        }
+
+    } while (u != 6);
+
+    system("cls");
+    cout << "-----------------------" << endl;
+    cout << "\nThank you for trying this program" << endl;
+    cout << "\n-----------------------" << endl;
+    system("pause");
+    return 0;
 }
-
--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+## How to contribute to the project
+1. Fork the project: `In the top-right corner of the page, click Fork.`
+2. Clone the project which you have just forked to your computer
+```
+git clone https://github.com/YOUR_GITHUB_USERNAME/PROJECT_NAME
+```
+3.  Make a new branch `(You can use issue, patch-number or your username as the branch name)`
+```
+  git checkout -b <name-of-new-branch>
+```
+4. Code and make your changes to the code
+
+5. Stage your changes:
+```
+git add FILENAMES
+```
+6. Commit:
+```
+git commit -m "your message"
+```
+7. Push:
+```
+git push origin <name-of-your-branch>
+```
+8. Once done navigate to your forked repo and click `Compare & Pull Request` button.
+    * This button would bring you to a new page and now you can click on a green button `Create Pull Request`. After creating a Pull Request `(PR)` the moderators shall go through your code and merge to the main branch of the project.
+
+---
+
+
+
 ## Projects Ideas
 ```
 1. Banking system with all banking facilities like – deposit, withdrawal, foreign exchange to any currency, availability of loans for purchasing vehicles, apartments, houses, setting up business, education loan, management of ATMs and all other features.
