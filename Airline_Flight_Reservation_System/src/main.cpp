a02848b62b5a493645f90d681d253ff903950153
Disha Modi <bhavnamodi802@gmail.com>
2023-04-09 09:40:09 +0530

    Creating the directory structure

---
+++ Airline_Flight_Reservation_System/src/main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Airline flight reservation system (online booking of tickets in different flights for different destinations all over the world, cancellation of tickets, clear display of cancellation amount, refund of amount after cancellation, showing  availability of all flights, showing flights timings for all 7 days of a week, seats availability, seat selection for travelers by giving the complete layout of the seating arrangement inside the flights, food availability/non-availability inside the flights, change of travel dates and amount charged.)
+*/
+void welcome()
+{
+	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                                                  |" << "\n"; 
+	std::cout << "\t\t|     WELCOME TO YRNCOLLO AIRLINE FLIGHT RESERVATION SYSTEM        |" << "\n";
+	std::cout << "\t\t|                                                                  |" << "\n"; 
+	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
+	std::cout << "\t\t|          Choose your option:                                     |" << "\n"; 
+	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
+	std::cout << "\t\t|                                                                  |" << "\n";  
+	std::cout << "\t\t|          1) BOOK TICKET                                          |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n"; 
+	std::cout << "\t\t|          2) CANCEL TICKET                                        |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          3) CHANGE RESERVATION                                   |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          4) PASSENGER DETAILS                                    |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          5) GET FLIGHT DETAILS                                   |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          6) ALL AVAILABLE FLIGHTS                                |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          7) BOOK TICKET                                          |" << "\n"; 
+	std::cout << "\t\t|                                                                  |" << "\n";
+	std::cout << "\t\t|          8) EXIT                                                 |" << "\n"; 
+	std::cout << "\t\t|------------------------------------------------------------------|" << "\n";
+
+}
+
+int main()
+{
+    welcome();
+    return 0;
+}
